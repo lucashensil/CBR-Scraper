fa3602fd088362a95f133ffe142a13e527e97b63
Lucas Henrique Silva <159385729+lucashensil@users.noreply.github.com>
2024-06-02 17:54:58 -0300

    Base of the news DataFrame created

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ url = 'https://www.cbr.com/category/movies/news-movies/'
 request = requests.get(url)
 soup = BeautifulSoup(request.text, 'html.parser')
 
-articles = soup.find_all(class_='article')
+dic_news = {'Titulo': [], 'Autor': [], 'Tag': [], 'Descricao': [], 'Link': []}
 
+articles = soup.find_all(class_='article')
+noticia = 1
 for article in articles:
+    if noticia == 21:
+        break
     article_title = article.find(class_='display-card-title').text.strip()
     article_author = article.find(class_='article-author').text
     article_tag = article.find(class_='dc-tag-label').text
@@ -18,6 +22,22 @@ for article in articles:
     article_request = requests.get(article_link)
     article_soup = BeautifulSoup(article_request.text, 'html.parser')
     article_description = article_soup.find(class_='content-block-regular').find('p').text.strip()
+    print(f'{noticia}º noticia pega')
+    noticia += 1
+    dic_news['Titulo'].append(article_title)
+    dic_news['Autor'].append(article_author)
+    dic_news['Tag'].append(article_tag)
+    dic_news['Descricao'].append(article_description)
+    dic_news['Link'].append(article_link)
 
-  
+
+df_news = pd.DataFrame(dic_news)
+df_news.to_excel('test.xlsx')
+
+
+
+
+
+
+
 
